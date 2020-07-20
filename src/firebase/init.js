7766792e7aae02aebe80d9a4dfa3c8068681b2d3
Syed Adzha <adzhamuhaymin@gmail.com>
2020-07-20 17:11:08 +0800

    remove config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,13 +3,11 @@ import firestore from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyBfqnekI7NWPdveYmo7UnZ_5lPp7QQ5n3I",
-  authDomain: "geo-location-22b63.firebaseapp.com",
-  databaseURL: "https://geo-location-22b63.firebaseio.com",
-  projectId: "geo-location-22b63",
-  storageBucket: "geo-location-22b63.appspot.com",
-  messagingSenderId: "707635298363",
-  appId: "1:707635298363:web:7ea6cf651fc20e30b51c1e"
+  	// ################################################################################
+  	
+  	// Include Firebase configuration
+  	
+  	// ################################################################################
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
